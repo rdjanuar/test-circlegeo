9da5c1543166dd341c06103bc5a86cd8046fa6c4
rdjanuar <rdjanuar008@gmail.com>
2024-12-12 00:00:11 +0700

    feat(layer/cicrle): implement component

--- B/nuxt.config.ts
+++ B/nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"]
-})
+  modules: ["@nuxt/ui", "nuxt-maplibre"],
+  ssr: false,
+  compatibilityDate: "2024-12-11",
+});
